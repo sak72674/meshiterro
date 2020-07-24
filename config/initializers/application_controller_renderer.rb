aa4a7d0723d73c8f28fdb27728630f8590ad405c
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-07-24 21:53:30 +0900

    Finish 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ae0eb99d32ce47ca6a4f22dffc1dec31f2ade425153a4c816a8047a26f4eaddc2da45ec8a8ecd00454a099010f16b4f80bdf58cc9f8b1fbee17281db708c0d21'
